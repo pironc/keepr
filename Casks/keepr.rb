5e7a739766d5c66752c6faad02b645f8b00594cc
Cyprien Piron <cyprien.piron@gmail.com>
2026-08-20 00:23:16 +0200

    updated quarantine flag warning for cask

--- Casks/keepr.rb
+++ Casks/keepr.rb
@@ -10,4 +10,11 @@ cask "keepr" do
   homepage "https://github.com/pironc/keepr"
 
   app "keepr.app"
+
+  caveats <<~EOS
+    keepr isn't code-signed or notarized yet, so macOS will say the app
+    "is damaged and can't be opened" on first launch. This clears the
+    quarantine flag rather than actually repairing anything broken:
+      xattr -cr #{appdir}/keepr.app
+  EOS
 end
